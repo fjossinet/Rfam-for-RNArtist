3016050675602d506a0e308f07d071abf1524b67
Fabrice Jossinet <fjossinet@gmail.com>
2022-01-20 20:29:41 +0100

    Added family RF00005

---
+++ data/RF00005/rnartist.kts
@@ -0,0 +1,116 @@
+import io.github.fjossinet.rnartist.core.*      
+
+rnartist {
+    ss {
+        rfam {
+            id = "RF00005"
+            name = "consensus"
+            use alignment numbering
+        }
+    }
+    theme {
+        details { 
+            value = 3
+        }
+
+        color {
+            location {
+                1 to 7
+                110 to 116
+            }
+            value = "#385967"
+        }
+
+
+        color {
+            location {
+                12 to 15
+                29 to 32
+            }
+            value = "#76022c"
+        }
+
+
+        color {
+            location {
+                37 to 40
+                49 to 52
+            }
+            value = "#985a8e"
+        }
+
+
+        color {
+            location {
+                87 to 88
+                105 to 106
+            }
+            value = "#1691b4"
+        }
+
+
+        color {
+            location {
+                90 to 91
+                103 to 104
+            }
+            value = "#a9343d"
+        }
+
+
+
+        color {
+            location {
+                8 to 11
+                33 to 36
+                53 to 86
+                107 to 109
+            }
+            value = "#9fc581"
+        }
+
+
+        color {
+            location {
+                16 to 28
+            }
+            value = "#57adc5"
+        }
+
+
+        color {
+            location {
+                41 to 48
+            }
+            value = "#240ce7"
+        }
+
+
+        color {
+            location {
+                89 to 89
+                105 to 104
+            }
+            value = "#84ced0"
+        }
+
+
+        color {
+            location {
+                92 to 102
+            }
+            value = "#207b81"
+        }
+
+
+
+        color {
+            location {
+                117 to 118
+            }
+            value = "#9ae12d"
+        }
+
+
+    }
+}           
